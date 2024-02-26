1eac6d7fadc45d86d0b82b563f16a05f611bad6e
noahzamarripa <91333145+noahzamarripa@users.noreply.github.com>
2024-02-25 20:11:09 -0600

    basic skeleton of login and creat account page

--- main-react-app/src/App.js
+++ main-react-app/src/App.js
@@ -1,10 +1,24 @@
+import React, {useState} from "react";
 import "./App.css"
 import PromptForm from "./PromptForm";
+import { LoginForm } from "./LoginForm";
+import { CreateAccountForm } from "./CreateAccountForm";
 
 function App() {
+  //create state of current form
+  const [currentForm, setForm] = useState('LoginForm');
+
+  // take in form name to set current form to correct one
+  const changeForm = (formName) => {
+    setForm(formName);
+  }
+
   return (
     <div className="App">
-      <PromptForm /> 
+      {
+        // if login is current form then go to the LoginForm otherwise go to CreateAccountForm
+        currentForm === 'LoginForm' ? <LoginForm onformSwitch = {changeForm} /> : <CreateAccountForm onformSwitch = {changeForm} />
+      } 
     </div>
   );
 }
